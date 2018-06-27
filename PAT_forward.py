97aa05b3bdd661ce889e7ade679bd006608422d3
Sheroze Sheriffdeen <mss385@cornell.edu>
2018-06-27 22:40:23 +0000

    Added python gitignore stuff

--- PAT_forward.py
+++ PAT_forward.py
@@ -1,56 +1,55 @@
-from __future__ import absolute_import, division, print_function
-
-from hippylib import nb
-import dolfin as dl
-import math
-import numpy as np
-import matplotlib.pyplot as plt
 import sys
-import os
+sys.path.insert(0,'/home/fenics/Installations/MUQ_INSTALL/lib')
+import pymuqModeling as mm
+import dolfin as dl
 
+class PAT_forward(mm.PyModPiece):
+    
+    def __init__(self, time_final, numSteps, c, V):
+        """ 
+        INPUTS:
+        
+        
+        """
+        mm.PyModPiece.__init__(self, [V.dim()],[V.dim()])
+                  
+        self.T_f = time_final
+        self.numSteps = numSteps
+        self.dt = self.T_f / self.numSteps
+        self.c = c
+        self.V = V
+        
+        self.p_trial = dl.TrialFunction(self.V)
+        self.v = dl.TestFunction(self.V)
+            
+    def EvaluateImpl(self, inputs):
+        """
+        
+        """
+        m = dl.Function(self.V)
+        m.vector().set_local(inputs[0])
+        
+        p_n = dl.interpolate(m, self.V)
+        p_nm1 = dl.interpolate(m, self.V)
+        p_trial = self.p_trial
+        v = self.v
+        
+        F = (self.c**2)*(self.dt**2)*dl.inner(dl.grad(p_trial), dl.grad(v))*dl.dx - 2.*p_n*v*dl.dx + p_trial*v*dl.dx + p_nm1*v*dl.dx 
+        a, L = dl.lhs(F), dl.rhs(F)
+        
+        # Time-stepping
+        p = dl.Function(self.V)
+        t = 0
+        
+        for n in range(self.numSteps):
+            # Update current timtime
+            t += self.dt
 
-T = .005 # final time
-num_steps = 10000 # number of time steps
-dt = T / num_steps # time step size
-c = 1500    #speed of sound
+            # Compute solution
+            dl.solve(a == L, p)
 
-# Create mesh and define function space
-nx = ny = 60
-mesh = dl.RectangleMesh(dl.Point(0, 0),dl.Point(10,10),nx,ny,"right")
-V = dl.FunctionSpace(mesh, 'P', 1)
-# Define boundary condition
-m = dl.interpolate(dl.Expression('std::log( 8. - 4.*(pow(x[0] - 5.,2) + pow(x[1] - 5.,2) < pow(0.2,2) ) )', degree=5), V)#dl.Constant(1.0)
-p_n = dl.interpolate(m, V)
-p_nm1 = dl.interpolate(m, V)
-nb.plot(p_n, vmin=1., vmax = 3.)
-plt.show()
-#p_n = dl.project(m, V)
-# Define variational problem
-p = dl.TrialFunction(V)
-v = dl.TestFunction(V)
-#f = dl.Constant(beta - 2 - 2*alpha)
-F = c**2*dt**2*dl.inner(dl.grad(p), dl.grad(v))*dl.dx - 2.*p_n*v*dl.dx + p*v*dl.dx + p_nm1*v*dl.dx 
-a, L = dl.lhs(F), dl.rhs(F)
-# Time-stepping
-p = dl.Function(V)
-t = 0
-for n in range(num_steps):
-    # Update current timtime
-    t += dt
-    #u_D.t = t
-    
-    # Compute solution
-    dl.solve(a == L, p)
-    
-    if (n % 100)== 0 :
-        nb.plot(p, vmin=1., vmax = 3.)
-        plt.show()
-    # Compute error at vertices
-#     u_e = dl.interpolate(u_D, V)
-#     error = np.abs(u_e.vector().array() - u.vector().array()).max()
-#     print('t = %.2f: error = %.3g' % (t, error))
-    # Update previous solution
-    p_nm1.assign(p_n)
-    p_n.assign(p)
-    
-  
+            # Update previous solution
+            p_nm1.assign(p_n)
+            p_n.assign(p)           
+        
+        self.outputs = [p.vector().get_local()]
